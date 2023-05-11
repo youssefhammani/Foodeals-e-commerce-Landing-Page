f2e44b609c507fcf310fb95b367a4bfc84f2cb0d
youssefhammani <youssefhammani04@gmail.comclear>
2023-05-11 10:15:20 +0100

    card best for you

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,5 +4,16 @@ $(document).ready(function(){
         dots: false,
         nav: true,
         margin: 30,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:3
+            },
+            1000:{
+                items:5
+            }
+        }
     });
-  });
+});
